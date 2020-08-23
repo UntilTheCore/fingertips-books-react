e971f72b382959c02e6155ab1d03167150f9764e
UntilTheCore <814898862@qq.com>
2020-08-23 23:47:23 +0800

    创建<Types />组件并完成样式布局。

---
+++ src/components/Types.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Props = {
+    type: string
+}
+
+const TypesSection = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: deepskyblue;
+    font-size: 18px;
+    font-weight: bold;
+    div {
+        position: relative;
+        padding: 12px 16px;
+        &.selected::after {
+            content: '';
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            border: 1px solid #333;
+        }
+    }
+`;
+
+const Types = (props: Props) => {
+    return (
+        <TypesSection>
+            <div className={props.type === '-' ? 'selected' : ''}>支出</div>
+            <div className={props.type === '+' ? 'selected' : ''}>收入</div>
+        </TypesSection>
+    );
+};
+
+export default Types;
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import Types from '../components/Types';
 
 const Money = () => {
     return (
         <Layout>
+            <Types type='-' />
             <h2>记账页面</h2>
         </Layout>
     );
