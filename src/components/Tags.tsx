97c35d584043f778ce9e1f15aee7a70c7b625029
UntilTheCore <814898862@qq.com>
2020-08-24 14:50:47 +0800

    完成<Tags /> 组件布局。

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import Icon from './Icon';
 
-const Ul = styled.ul`
+const TagsSection = styled.section`
     display: flex;
     flex-wrap: wrap;
-    > li {
+    > div {
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -12,8 +13,10 @@ const Ul = styled.ul`
         width: 25vw;
         height: 25vw;
         text-align: center;
-        outline: 1px solid red;
         > div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
             width: 50%;
             height: 50%;
             border-radius: 50%;
@@ -22,148 +25,39 @@ const Ul = styled.ul`
                 background-color: rgb(5,200,245);
             }
         }
+        
+        > span {
+            margin-top: 8px;
+            font-size: 14px;
+        }
     }
-`
+`;
 const Tags = () => {
+    const iconHashTable = new Map([
+        ['dining', '餐饮'],
+        ['shopping', '购物'],
+        ['bus', '交通'],
+        ['commodity', '日常']]);
+    const liArray: any = [];
+    iconHashTable.forEach((value, key) => {
+        liArray.push(
+            <div key={ key }>
+                <Icon name={ key } />
+                <span>{ value }</span>
+            </div>
+        );
+    });
     return (
-        <Ul>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-               <span>餐饮</span>
-            </li>
-            <li>
-                <div>
-                    123
+        <TagsSection>
+            { liArray }
+            <div>
+                <div className={ 'selected' }>
+                    <Icon name="dining" />
                 </div>
                 <span>餐饮</span>
-            </li>
-            <li>
-                <div>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>10</li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-            <li>
-                <div className={"selected"}>
-                    123
-                </div>
-                <span>餐饮</span>
-            </li>
-        </Ul>
-    )
-}
+            </div>
+        </TagsSection>
+    );
+};
 
 export default Tags;
