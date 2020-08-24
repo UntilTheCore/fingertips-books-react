59ef7c07cc15e6ce0ee633448ef63f676eefb3ee
UntilTheCore <814898862@qq.com>
2020-08-24 15:09:53 +0800

    创建并使用可复用组件<Tag />

---
+++ src/components/Tag.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Icon from './Icon';
+import styled from 'styled-components';
+
+type Props = {
+   iconName: string,
+   tagTitle: string,
+}
+
+const TagSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 25vw;
+    height: 25vw;
+    text-align: center;
+    > div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+        height: 50%;
+        border-radius: 50%;
+        background-color: #F5F5F5;
+        &.selected {
+            background-color: rgb(5,200,245);
+        }
+    }
+    
+    > span {
+        margin-top: 8px;
+        font-size: 14px;
+    }
+`
+
+const Tag = (props: Props) => {
+    return (
+        <TagSection>
+            <div>
+                <Icon name={ props.iconName } />
+            </div>
+            <span>{ props.tagTitle }</span>
+        </TagSection>
+    );
+};
+
+export default Tag;
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,36 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import Icon from './Icon';
+import Tag from './Tag';
 
 const TagsSection = styled.section`
     display: flex;
     flex-wrap: wrap;
-    > div {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 25vw;
-        height: 25vw;
-        text-align: center;
-        > div {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 50%;
-            height: 50%;
-            border-radius: 50%;
-            background-color: #F5F5F5;
-            &.selected {
-                background-color: rgb(5,200,245);
-            }
-        }
-        
-        > span {
-            margin-top: 8px;
-            font-size: 14px;
-        }
-    }
 `;
 const Tags = () => {
     const iconHashTable = new Map([
@@ -40,22 +14,12 @@ const Tags = () => {
         ['commodity', '日常']]);
     const liArray: any = [];
     iconHashTable.forEach((value, key) => {
-        liArray.push(
-            <div key={ key }>
-                <Icon name={ key } />
-                <span>{ value }</span>
-            </div>
-        );
+        liArray.push(<Tag key={ key } iconName={ key } tagTitle={ value } />);
     });
     return (
         <TagsSection>
             { liArray }
-            <div>
-                <div className={ 'selected' }>
-                    <Icon name="dining" />
-                </div>
-                <span>餐饮</span>
-            </div>
+            <Tag iconName="dining" tagTitle="餐饮" />
         </TagsSection>
     );
 };
