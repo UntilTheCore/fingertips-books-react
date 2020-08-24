34adc7693b2ad032dd0686016790c3a53341a010
UntilTheCore <814898862@qq.com>
2020-08-24 10:13:50 +0800

    优化使用体验，兼容pc和iOS

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,13 +3,20 @@ import styled from 'styled-components';
 import Nav from './Nav';
 
 const Wrapper = styled.div`
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      height: calc(var(--vh, 1vh));
-      > main {
-          overflow: auto;
-      }
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: calc(var(--vh, 1vh));
+    > main {
+        overflow: auto;
+        // 使 iOS 端滑动顺畅
+        -webkit-overflow-scrolling: touch;
+        // 隐藏滚动条
+        &::-webkit-scrollbar {
+            height: 0 !important;
+            width: 0 !important;
+        }
+    }
 `;
 
 const Layout = (props: any) => {
