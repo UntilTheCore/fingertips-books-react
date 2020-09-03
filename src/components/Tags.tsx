ce1c6c689ae8d4ffbbc5900e4e95d3aca2502afa
UntilTheCore <814898862@qq.com>
2020-09-03 09:46:12 +0800

    实现<Tags />组件的标签选中状态数据的获取。

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -6,55 +6,26 @@ const TagsSection = styled.section`
     display: flex;
     flex-wrap: wrap;
 `;
-const Tags = () => {
-    const iconHashTable = new Map([
-        ['dining', '餐饮'],
-        ['shopping', '购物'],
-        ['bus', '交通'],
-        ['commodity', '日常'],
-        ['vegetables', '蔬菜'],
-        ['fruits', '水果'],
-        ['snacks', '零食'],
-        ['costume', '服饰'],
-        ['amusement', '娱乐'],
-        ['redpacket', '红包'],
-        ['communication', '通讯'],
-        ['socialContact', '社交'],
-        ['sport', '运动'],
-        ['beauty', '美容'],
-        ['travel', '旅游'],
-        ['gift', '礼物'],
-        ['house', '住房'],
-        ['car', '汽车'],
-        ['living', '居家'],
-        ['homeAppliances', '家电'],
-        ['waterAndElectricity', '水电'],
-        ['expressage', '快递'],
-        ['repair', '维修'],
-        ['eldership', '长辈'],
-        ['child', '孩子'],
-        ['wine', '烟酒'],
-        ['health', '医疗'],
-        ['pet', '宠物'],
-        ['study', '学习'],
-        ['book', '书籍'],
-        ['office', '办公'],
-        ['donate', '捐赠'],
-        ['lottery', '彩票'],
-        ['other', '其他支出']
-    ]);
-    // TODO setTags 用来后期新增标签用,现在只用预设标签
-    const [tags, setTags] = useState(iconHashTable);
+
+type Props = {
+    tags: Map<string,string>,
+    onChange: (tagId: string) => void,
+}
+
+const Tags: React.FC<Props> = (props) => {
     const [selectedTag, setSelectedTag] = useState<string>();
     const liArray: any = [];
-    const onToggleTag = (tagName: string) => {
-        if(selectedTag === tagName) {
+    const onToggleTag = (tagId: string) => {
+        if(selectedTag === tagId) {
+            // 自己被再次点击，则取消选中状态
            setSelectedTag('')
         }else{
-            setSelectedTag(tagName);
+            // 标签被选中了
+            setSelectedTag(tagId);
+            props.onChange(tagId)
         }
     };
-    tags.forEach((value, key) => {
+    props.tags.forEach((value, key) => {
         liArray.push(<Tag isSelected={ selectedTag === key } onClick={ () => onToggleTag(key) } key={ key }
                           iconName={ key } tagTitle={ value } />);
     });
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -7,9 +7,48 @@ import Layout from '../components/Layout';
 
 const Money = () => {
     const [selectedType,setSelectedType] = useState<'-'|'+'>('-');
+    const [selectedTag,setSelectedTag] = useState('')
+    const iconHashTable = new Map([
+        ['dining', '餐饮'],
+        ['shopping', '购物'],
+        ['bus', '交通'],
+        ['commodity', '日常'],
+        ['vegetables', '蔬菜'],
+        ['fruits', '水果'],
+        ['snacks', '零食'],
+        ['costume', '服饰'],
+        ['amusement', '娱乐'],
+        ['redpacket', '红包'],
+        ['communication', '通讯'],
+        ['socialContact', '社交'],
+        ['sport', '运动'],
+        ['beauty', '美容'],
+        ['travel', '旅游'],
+        ['gift', '礼物'],
+        ['house', '住房'],
+        ['car', '汽车'],
+        ['living', '居家'],
+        ['homeAppliances', '家电'],
+        ['waterAndElectricity', '水电'],
+        ['expressage', '快递'],
+        ['repair', '维修'],
+        ['eldership', '长辈'],
+        ['child', '孩子'],
+        ['wine', '烟酒'],
+        ['health', '医疗'],
+        ['pet', '宠物'],
+        ['study', '学习'],
+        ['book', '书籍'],
+        ['office', '办公'],
+        ['donate', '捐赠'],
+        ['lottery', '彩票'],
+        ['other', '其他支出']
+    ]);
     return (
         <div>
             {selectedType}
+            <br/>
+            {selectedTag}
             <Layout
                 headerSlot={
                     <Types
@@ -26,7 +65,7 @@ const Money = () => {
                         <NumberPad key={'a'} className={'xxx'}/>
                     </QueueAnim>
                 }>
-                <Tags />
+                <Tags tags={iconHashTable} onChange={(tagId) => {setSelectedTag(tagId)}} />
             </Layout>
         </div>
     );
