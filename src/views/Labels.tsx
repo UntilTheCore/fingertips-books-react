95eb92db05bb27f70373c6cfda195b1034658fe6
untilthecore <814898862@qq.com>
2021-09-03 14:07:40 +0800

    <TagList />样式完成

--- src/views/Labels.tsx
+++ src/views/Labels.tsx
@@ -1,19 +1,68 @@
 // 统计页面
 import React from "react";
+import styled from "styled-components";
 import Layout from "components/Layout";
 import { useTags } from "hook/useTags";
+import Icon from "components/Icon";
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+
+  > li {
+    //#e5e5e7
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    margin-left: 16px;
+    padding: 12px 16px 12px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    > a {
+      padding: 12px 16px 12px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+`;
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #f60;
+  border-radius: 4px;
+  color: white;
+`;
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Space = styled.div`
+  height: 16px;
+`;
 
 const Labels = () => {
   const {tags, setTags} = useTags();
   return (
     <Layout name="标签页面">
-      <ul>
-        {
-          tags.map(item =>
-            <li key={ item }>{ item }</li>
-          )
-        }
-      </ul>
+      <TagList>
+        { tags.map(tag =>
+          <li key={ tag }>
+            <span className="oneLine">{ tag }</span>
+            <Icon name="right" />
+          </li>
+        ) }
+      </TagList>
+      <Center>
+        <Space />
+        <Space />
+        <Space />
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 };
