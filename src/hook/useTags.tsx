160843cea58414e319b1d015246f6a091fb75696
untilthecore <untilthecore@gmail.com>
2021-09-04 19:38:57 +0800

    解决创建的标签id异常的问题

--- src/hook/useTags.tsx
+++ src/hook/useTags.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import CreateId from "../lib/createId";
 
+const _tags = [
+  { id: (new CreateId()).getId(), name: "衣" },
+  { id: (new CreateId()).getId(), name: "食" },
+  { id: (new CreateId()).getId(), name: "住" },
+  { id: (new CreateId()).getId(), name: "行" },
+]
+
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number, name: string }[]>([
-    {id: (new CreateId()).getId(), name: "衣"},
-    {id: (new CreateId()).getId(), name: "食"},
-    {id: (new CreateId()).getId(), name: "住"},
-    {id: (new CreateId()).getId(), name: "行"},
-  ]);
-  return {tags, setTags};
+  const [tags, setTags] = useState<{ id: number, name: string }[]>(_tags);
+  return { tags, setTags };
 };
 
 export { useTags };
