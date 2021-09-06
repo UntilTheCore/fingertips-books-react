2b64cf0fdd2368c9a7712dbb5e23cb46ee96aef8
untilthecore <814898862@qq.com>
2021-09-06 23:34:38 +0800

    完成<TopBar />按钮返回功能

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { SVGAttributes } from "react";
+import cs from "classnames";
 // 防止被tree shaking，使用require引入svg
 const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 
 importAll(require.context("icon", true, /\.svg$/));
 
-const Icon = (props: { name: string }) => {
+type Props = {
+  name: string,
+} & SVGAttributes<SVGElement>;
+
+const Icon = (props: Props) => {
+  const { name, children, className, ...rest } = props;
   return (
-    <svg className={ "icon" } aria-hidden={ true }>
-      <use xlinkHref={ "#" + props.name } />
+    <svg className={cs("icon", className)} {...rest} >
+      <use xlinkHref={"#" + props.name} />
     </svg>
   );
 };
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Icon from "./Icon";
+import { useHistory } from "react-router-dom";
 
 const TopBarWrapper = styled.header`
   display:flex;
@@ -19,10 +20,16 @@ type Props = {
 }
 
 const TopBar: React.FC<Props> = (props) => {
+
+  const history = useHistory();
+  const back = () => {
+    history.length > 2 ? history.goBack() : history.push('/');
+  }
+
   return (
     <TopBarWrapper>
       {
-        props.enableBack && <Icon name="left" />
+        props.enableBack && <Icon name="left" onClick={back} />
       }
       <span>{props.title}</span>
       <Icon name="" />
