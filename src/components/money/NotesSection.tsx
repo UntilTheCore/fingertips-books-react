210f2a6924239907000ae12928a19a69b251259d
untilthecore <814898862@qq.com>
2021-09-08 15:59:45 +0800

    备注面板数据收集

--- src/components/money/NotesSection.tsx
+++ src/components/money/NotesSection.tsx
@@ -1,5 +1,5 @@
 import Input from "components/Input";
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 const MyNotesSection = styled.section`
@@ -10,7 +10,7 @@ const MyNotesSection = styled.section`
 
 const InputWrapper = styled.section`
   padding: 12px 0;
-`
+`;
 
 type Props = {
   value: string,
@@ -18,17 +18,15 @@ type Props = {
 }
 
 const NotesSection: React.FC<Props> = (props) => {
-  const [note, setNote] = useState('');
-
   // 这里onChange的类型应使用 React.ChangeEventHandler<HTMLInputElement> 来为其设置类型，但可以不设置在函数而设置到参数的类型上，两种方式都能正确获取value，但最好还是给函数设置类型而不是在参数上。
   const onChange: React.ChangeEventHandler<HTMLInputElement> = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNote(e.target.value);
+    props.onChange(e.target.value);
   };
 
   return (
     <MyNotesSection>
       <InputWrapper>
-        <Input title="备注" value={note} placeholder="请填写备注" onChange={onChange} />
+        <Input title="备注" defaultValue="" placeholder="请填写备注" onChange={ onChange } />
       </InputWrapper>
     </MyNotesSection>
   );
