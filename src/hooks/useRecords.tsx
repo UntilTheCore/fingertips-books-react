54cfb558bd9554e9826c88497efcad0ffe8d24c6
untilthecore <814898862@qq.com>
2021-09-08 16:27:43 +0800

    新增获取本地record数据hook

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useEffect, useState } from "react";
 import { useUpdate } from "./useUpdate";
 
 type recordType = {
@@ -14,8 +14,17 @@ type newRecordType = Omit<recordType, "createAt">;
 const useRecords = () => {
   const [records, setRecords] = useState<recordType[]>([]);
 
+  // 获取 records 数据
+  useEffect(() => {
+    setRecords(JSON.parse(localStorage.getItem("records") || "[]"));
+  }, []);
+
+  useUpdate(() => {
+    localStorage.setItem("records", JSON.stringify(records));
+  }, [records]);
+
+
   const addRecord = (record: newRecordType) => {
-    console.log("add record");
     if (record.money <= 0) {
       alert("金额不能为0");
       return false;
@@ -30,13 +39,7 @@ const useRecords = () => {
     setRecords([...records, temp]);
   };
 
-  const a = useCallback(() => {
-    localStorage.setItem("records", JSON.stringify(records));
-  }, [records]);
-
-  useUpdate(a, [records]);
-
-  return {addRecord};
+  return {records, addRecord};
 };
 
 export { useRecords };
