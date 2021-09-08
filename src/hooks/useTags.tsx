aa47240a63d865e1dc6070518e09418ad0a786e5
untilthecore <814898862@qq.com>
2021-09-08 18:03:03 +0800

    获取收支列表

--- src/hooks/useTags.tsx
+++ src/hooks/useTags.tsx
@@ -14,14 +14,14 @@ const useTags = () => {
 
   useEffect(() => {
     // 获取持久化数据
-    const localTags: tagType[] = JSON.parse(localStorage.getItem('tags') || '[]');
+    const localTags: tagType[] = JSON.parse(localStorage.getItem("tags") || "[]");
     if (localTags.length === 0) {
       const defaultTags: tagType[] = [
-        { id: (new CreateId()).getId(), name: "衣" },
-        { id: (new CreateId()).getId(), name: "食" },
-        { id: (new CreateId()).getId(), name: "住" },
-        { id: (new CreateId()).getId(), name: "行" },
-      ]
+        {id: (new CreateId()).getId(), name: "衣"},
+        {id: (new CreateId()).getId(), name: "食"},
+        {id: (new CreateId()).getId(), name: "住"},
+        {id: (new CreateId()).getId(), name: "行"},
+      ];
       setTags(defaultTags);
     } else {
       setTags(localTags);
@@ -29,20 +29,20 @@ const useTags = () => {
   }, []);
 
   useUpdate(() => {
-    localStorage.setItem('tags', JSON.stringify(tags));
+    localStorage.setItem("tags", JSON.stringify(tags));
   }, [tags]);
 
   const findTag: findTagType = (tagId: number): tagType | undefined => {
     return tags.filter(tag => tag.id === tagId)[0];
-  }
+  };
 
   // 添加新的标签
   const addTag = () => {
     const newTag = window.prompt("请输入新的标签名:");
     if (newTag && newTag.trim()) {
-      setTags([...tags, { id: (new CreateId()).getId(), name: newTag }]);
+      setTags([...tags, {id: (new CreateId()).getId(), name: newTag}]);
     }
-  }
+  };
 
   /**
    * 根据tag id找在 _tags 中的下标
@@ -58,7 +58,7 @@ const useTags = () => {
       }
     }
     return result;
-  }
+  };
 
   /**
    * 根据标签的id和提供含name的对象更新tag名
@@ -66,16 +66,16 @@ const useTags = () => {
    * @param obj
    * @returns
    */
-  const updateTags = (tagId: number, { name }: { name: string }) => {
+  const updateTags = (tagId: number, {name}: { name: string }) => {
     const index = findTagIndex(tagId);
     if (index !== -1) {
       setTags(tags.map(tag => {
-        return tag.id === tagId ? { id: tag.id, name } : tag;
+        return tag.id === tagId ? {id: tag.id, name} : tag;
       }));
       return true;
     }
     return false;
-  }
+  };
 
   /**
    * 根据tag id 删除标签
@@ -90,9 +90,17 @@ const useTags = () => {
       return true;
     }
     return false;
-  }
+  };
 
-  return { tags, setTags, addTag, findTag, findTagIndex, updateTags, deleteTag };
+  /**
+   * 根据标签id查询标签的名称
+   * @param tagId
+   */
+  const findTagName = (tagId: number) => {
+    return tags.filter(tag => tag.id === tagId)[0].name;
+  };
+
+  return {tags, setTags, findTagName, addTag, findTag, findTagIndex, updateTags, deleteTag};
 };
 
 export { useTags };
--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,11 +1,62 @@
 // 统计页面
-import React from "react";
+import React, { useState } from "react";
 import Layout from "components/Layout";
+import { useRecords } from "hooks/useRecords";
+import { CategorySection } from "components/money/CategorySection";
+import styled from "styled-components";
+import { useTags } from "hooks/useTags";
+import dayjs from "dayjs";
+
+
+const CategorySectionWrapper = styled.section`
+  background-color: #f5f5f5;
+`;
+
+type recordType = {
+  tags: number[],
+  note: string,
+  category: "+" | "-",
+  money: number,
+  createAt: string, // 创建记录的日期
+}
 
 const Statistics = () => {
+  const {records} = useRecords();
+  const [category, setCategory] = useState<"+" | "-">("-");
+  const {tags} = useTags();
+
+  const categoryList: { name: string, list: recordType[] }[] = tags.map(t => {
+    return {
+      name: t.name,
+      list: records.filter(r => r.tags[0] === t.id)
+    };
+  }).filter(c => c.list.length > 0);
+
+  console.log(categoryList);
+
   return (
     <Layout name="统计页面">
-      <span>123</span>
+      <CategorySectionWrapper>
+        <CategorySection value={ category } onChange={ (v) => setCategory(v) } />
+      </CategorySectionWrapper>
+      {
+        categoryList.map(c => {
+          return (
+            <dl key={ c.name }>
+              <dt>{ c.name }</dt>
+              {
+                c.list.map((r, i) =>
+                  <dd key={ i }>
+                    <span>{ r.money }</span>
+                    &nbsp;&nbsp;
+                    <span>{ dayjs(r.createAt).format("YYYY.MM.DD") }</span>
+                  </dd>
+                )
+              }
+            </dl>
+          );
+        })
+      }
     </Layout>
   );
 };
